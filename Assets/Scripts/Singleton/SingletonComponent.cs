8e29fcd413df872674fa78fcbb19cd2fa1cd46a6
WangHan <henryok123@163.com>
2022-06-27 01:31:13 +0800

    增加：单例模式

--- Assets/Scripts/Singleton/SingletonComponent.cs
+++ Assets/Scripts/Singleton/SingletonComponent.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingletonComponent : MonoBehaviour
+public class SingletonComponent<T> : MonoBehaviour where T:SingletonComponent<T>
 {
-    // Start is called before the first frame update
-    void Start()
+  private static T _instance;
+  public static SingletonComponent<T> _Instance
+  {
+    get
     {
-        
+      if (!_instance)
+      {
+        T[] managers = GameObject.FindObjectsOfType(typeof(T)) as T[];
+        if(managers != null)
+        {
+          if(managers.Length == 1)
+          {
+            _instance = managers[0];
+            return _instance;
+          }else if(managers.Length > 1)
+          {
+            Debug.LogError("You have more than one " + typeof(T).Name + " in the Scene.It's a singleton");
+            for(int i = 0; i < managers.Length; ++i)
+            {
+              T manager = managers[i];
+              Destroy(manager.gameObject);
+            }
+          }
+        }
+        GameObject go = new GameObject(typeof(T).Name, typeof(T));
+        _instance = go.GetComponent<T>();
+        DontDestroyOnLoad(_instance.gameObject);
+        return _instance;
+      }
     }
-
-    // Update is called once per frame
-    void Update()
+    set
     {
-        
+      _instance = value as T;
     }
+  }
 }
